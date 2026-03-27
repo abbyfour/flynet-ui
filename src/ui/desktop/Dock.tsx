b20b8a286851f625922a496f6839eb021599ad40
Abby Ivison <jri1408@gmail.com>
2026-03-27 04:02:45 -0700

    Restyle dock

--- src/ui/desktop/Dock.tsx
+++ src/ui/desktop/Dock.tsx
@@ -1,10 +1,5 @@
 import "./Dock.scss";
 
-// icons
-import flightsIcon from "../../assets/dock/flights.svg";
-import friendsIcon from "../../assets/dock/friends.svg";
-import profileIcon from "../../assets/dock/profile.svg";
-import settingsIcon from "../../assets/dock/settings.svg";
 import { SidepanelWindows } from "../../data/classes/ui";
 import { useAppDispatch, useAppSelector } from "../../data/store";
 import {
@@ -12,22 +7,69 @@ import {
   setActiveSidepanelWindow,
 } from "../../data/uiSlice";
 
+// icons
+import flightsIconDarkBw from "../../assets/dock/flights-bw-dark.svg";
+import flightsIconBw from "../../assets/dock/flights-bw.svg";
+import flightsIconDark from "../../assets/dock/flights-dark.svg";
+import flightsIcon from "../../assets/dock/flights.svg";
+import friendsIconDarkBw from "../../assets/dock/friends-bw-dark.svg";
+import friendsIconBw from "../../assets/dock/friends-bw.svg";
+import friendsIconDark from "../../assets/dock/friends-dark.svg";
+import friendsIcon from "../../assets/dock/friends.svg";
+import profileIconDarkBw from "../../assets/dock/profile-bw-dark.svg";
+import profileIconBw from "../../assets/dock/profile-bw.svg";
+import profileIconDark from "../../assets/dock/profile-dark.svg";
+import profileIcon from "../../assets/dock/profile.svg";
+import settingsIconDarkBw from "../../assets/dock/settings-bw-dark.svg";
+import settingsIconBw from "../../assets/dock/settings-bw.svg";
+import settingsIconDark from "../../assets/dock/settings-dark.svg";
+import settingsIcon from "../../assets/dock/settings.svg";
+
 interface Window {
   name: SidepanelWindows;
   icon: string;
+  iconBw: string;
+  iconDark: string;
+  iconDarkBw: string;
 }
 
 const windows = {
-  flights: { name: SidepanelWindows.Flights, icon: flightsIcon },
-  friends: { name: SidepanelWindows.Friends, icon: friendsIcon },
-  profile: { name: SidepanelWindows.Profile, icon: profileIcon },
-  settings: { name: SidepanelWindows.Settings, icon: settingsIcon },
+  flights: {
+    name: SidepanelWindows.Flights,
+    icon: flightsIcon,
+    iconBw: flightsIconBw,
+    iconDark: flightsIconDark,
+    iconDarkBw: flightsIconDarkBw,
+  },
+  friends: {
+    name: SidepanelWindows.Friends,
+    icon: friendsIcon,
+    iconBw: friendsIconBw,
+    iconDark: friendsIconDark,
+    iconDarkBw: friendsIconDarkBw,
+  },
+  profile: {
+    name: SidepanelWindows.Profile,
+    icon: profileIcon,
+    iconBw: profileIconBw,
+    iconDark: profileIconDark,
+    iconDarkBw: profileIconDarkBw,
+  },
+  settings: {
+    name: SidepanelWindows.Settings,
+    icon: settingsIcon,
+    iconBw: settingsIconBw,
+    iconDark: settingsIconDark,
+    iconDarkBw: settingsIconDarkBw,
+  },
 } satisfies Record<string, Window>;
 
 export function Dock() {
   const activeWindow = useAppSelector(
     (state) => state.ui.activeSidepanelWindow,
   );
+  const theme = useAppSelector((state) => state.ui.theme);
+
   const dispatch = useAppDispatch();
 
   const changeWindow = (window: Window) => {
@@ -47,14 +89,25 @@ export function Dock() {
           onClick={() => changeWindow(window)}
         >
           <img
-            src={window.icon}
+            src={getIcon(window, activeWindow, theme)}
             alt={`${window.name} icon`}
             className="dock-icon-image"
-            height="70px"
-            width="70px"
+            width="65px"
           />
         </button>
       ))}
     </div>
   );
+}
+
+function getIcon(
+  window: Window,
+  activeWindow: SidepanelWindows | undefined,
+  theme: "light" | "dark",
+) {
+  if (activeWindow === window.name) {
+    return theme === "dark" ? window.iconDark : window.icon;
+  } else {
+    return theme === "dark" ? window.iconDarkBw : window.iconBw;
+  }
 }
--- src/ui/desktop/themeToggle/ThemeToggle.tsx
+++ src/ui/desktop/themeToggle/ThemeToggle.tsx
@@ -1,5 +1,10 @@
-import { AppTheme } from "../../../data/classes/ui";
 import { useAppDispatch, useAppSelector } from "../../../data/store";
+
+import darkToggleIcon from "../../../assets/icons/dark-toggle.svg";
+import lightToggleIcon from "../../../assets/icons/light-toggle.svg";
+
+import { Tooltip } from "@mantine/core";
+import { AppTheme } from "../../../data/classes/ui";
 import { setTheme } from "../../../data/uiSlice";
 import "./ThemeToggle.scss";
 
@@ -7,19 +12,26 @@ export function ThemeToggle() {
   const theme = useAppSelector((state) => state.ui.theme);
   const dispatch = useAppDispatch();
 
+  const toggleTheme = () => {
+    dispatch(
+      setTheme(theme === AppTheme.Dark ? AppTheme.Light : AppTheme.Dark),
+    );
+  };
+
   return (
     <div className="ThemeToggle">
-      <input
-        type="checkbox"
-        id="theme-toggle"
-        checked={theme === "dark"}
-        onChange={(e) =>
-          dispatch(setTheme(e.target.checked ? AppTheme.Dark : AppTheme.Light))
-        }
-      />
-      <label htmlFor="theme-toggle" className="toggle-label">
-        dark mode
-      </label>
+      <Tooltip
+        label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
+        openDelay={500}
+        position="right"
+      >
+        <button type="button" onClick={toggleTheme}>
+          <img
+            src={theme === "dark" ? lightToggleIcon : darkToggleIcon}
+            alt="Toggle theme"
+          />
+        </button>
+      </Tooltip>
     </div>
   );
 }
